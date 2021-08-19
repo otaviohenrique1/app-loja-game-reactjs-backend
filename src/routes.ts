d21dcbfd320fd8be0c8949296e946ca9ce4fa4f8
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-08-18 22:49:22 -0300

    Commit 9 dos arquivos

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ const routes = Router();
 routes.get('/usuarios', usuarioController.index);
 routes.get('/usuarios/:id', usuarioController.show);
 routes.post('/usuarios/login', usuarioController.login);
+routes.post('/usuarios', usuarioController.create);
 routes.put('/usuarios/:id', usuarioController.update);
 routes.delete('/usuarios/:id', usuarioController.delete);
 
