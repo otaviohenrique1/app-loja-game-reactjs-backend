5cbfb6696bb8dfe48212aac7c4b7d0eba2c5bef2
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-08-24 19:43:47 -0300

    Commit 17 dos arquivos

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
 import * as Yup from "yup";
 import Usuarios from "../entity/Usuarios";
 import usuarioView from "../views/UsuarioView";
@@ -104,28 +104,65 @@ export default {
     const usuario = await usuarioRepository.update(id, data);
     return response.status(201).json(usuario);
   },
-  async update2(request: Request, response: Response) {
-    const { id, nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado } = request.body;
-    const usuarioRepository = getRepository(Usuarios);
-    const data = { nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado };
-    const schema = Yup.object().shape({
-      nome: Yup.string().required(),
-      perfil: Yup.string().required(),
-      email: Yup.string().required(),
-      senha: Yup.string().required(),
-      sexo: Yup.string().required(),
-      data_nascimento: Yup.date().required(),
-      pais: Yup.string().required(),
-      cidade: Yup.string().required(),
-      estado: Yup.string().required(),
-      resumo: Yup.string().required(),
-      celular: Yup.string().required(),
-      url_personalizado: Yup.string().required(),
-    });
-    await schema.validate(data, {
-      abortEarly: false
-    });
-    const usuario = await usuarioRepository.update(id, data);
-    return response.status(201).json(usuario);
-  },
+  // async update2(request: Request, response: Response) {
+  //   const { id, nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado } = request.body;
+  //   const usuarioRepository = getRepository(Usuarios);
+  //   const data = { nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado };
+  //   const schema = Yup.object().shape({
+  //     nome: Yup.string().required(),
+  //     perfil: Yup.string().required(),
+  //     email: Yup.string().required(),
+  //     senha: Yup.string().required(),
+  //     sexo: Yup.string().required(),
+  //     data_nascimento: Yup.date().required(),
+  //     pais: Yup.string().required(),
+  //     cidade: Yup.string().required(),
+  //     estado: Yup.string().required(),
+  //     resumo: Yup.string().required(),
+  //     celular: Yup.string().required(),
+  //     url_personalizado: Yup.string().required(),
+  //   });
+  //   await schema.validate(data, {
+  //     abortEarly: false
+  //   });
+  //   const usuario = await usuarioRepository.update(id, data);
+  //   return response.status(201).json(usuario);
+  // },
+  // async update3(request: Request, response: Response) {
+  //   const { id, nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado } = request.body;
+  //   const data = { nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado };
+  //   const schema = Yup.object().shape({
+  //     nome: Yup.string().required('Campo nome esta invalido'),
+  //     perfil: Yup.string().required('Campo perfil esta invalido'),
+  //     email: Yup.string().required('Campo email esta invalido'),
+  //     senha: Yup.string().required('Campo senha esta invalido'),
+  //     sexo: Yup.string().required('Campo sexo esta invalido'),
+  //     data_nascimento: Yup.date().required('Campo data_nascimento esta invalido'),
+  //     pais: Yup.string().required('Campo pais esta invalido'),
+  //     cidade: Yup.string().required('Campo cidade esta invalido'),
+  //     estado: Yup.string().required('Campo estado esta invalido'),
+  //     resumo: Yup.string().required('Campo resumo esta invalido'),
+  //     celular: Yup.string().required('Campo celular esta invalido'),
+  //     url_personalizado: Yup.string().required('Campo url_personalizado esta invalido'),
+  //   });
+
+  //   await schema.validate(data, {
+  //     abortEarly: false
+  //   });
+
+  //   const usuario = await getConnection()
+  //     .createQueryBuilder()
+  //     .update(Usuarios)
+  //     .set(data)
+  //     .where("id = :id", { id: id })
+  //     .execute();
+    
+  //   return response.status(201).json(usuario);
+  // },
+  // async update4(request: Request, response: Response) {
+  //   const { id, nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado } = request.body;
+  //   const data = { id, nome, perfil, email, senha, sexo, data_nascimento, pais, cidade, estado, resumo, celular, url_personalizado };
+  //   console.log(data);
+  //   return response.status(201);
+  // },
 };
--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ routes.get('/usuarios/:id', usuarioController.show);
 routes.post('/usuarios/login', usuarioController.login);
 routes.post('/usuarios', usuarioController.create);
 routes.put('/usuarios/:id', usuarioController.update);
+// routes.put('/usuarios/edicao/:id', usuarioController.update4);
 routes.delete('/usuarios/:id', usuarioController.delete);
 
 export default routes;
